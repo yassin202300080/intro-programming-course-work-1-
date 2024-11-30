2c9bc8d194b2c115c1a1962fd383b460540439bf
yassin202300080 <yassin.elkholy@gmail.com>
2024-12-01 00:48:19 +0200

    Third Commit : Decoding and adding the output path paramter in the encoding function

--- programming CW1.py
+++ programming CW1.py
@@ -1,6 +1,6 @@
 # By: Yassin Ahmed, ID: 202300080
 
-def encode_message(image_path, message):
+def encode_message(image_path, message, output_path):
     # Read the BMP file
     with open(image_path, 'rb') as img:
         data = bytearray(img.read())
@@ -21,12 +21,47 @@ def encode_message(image_path, message):
             data[idx] = (data[idx] & 0xFE) | ((msg_bytes[i] >> (7 - bit)) & 0x01)
 
     # output to a new file
-    with open("Hidden.bmp", 'wb') as output:
+    with open(output_path, 'wb') as output:
         output.write(data)
 
-    print("Message successfully hidden in 'Hidden.bmp'")
+    print("Message successfully hidden in 'Hidden msg.bmp'")
+
+def decode_message(image_path):
+    #Read the BMP file 
+    with open(image_path, 'rb') as img:
+        data = bytearray(img.read())
+    
+    #extracting message from image's data
+    msg_bytes = bytearray()
+    #skiping the first 54 bytes
+    for i in range((len(data) - 54) // 8): 
+        byte = 0
+        for bit in range(8):
+            index = 54 + i * 8 + bit
+            byte = (byte << 1) | (data[index] & 0x01)
+        # Check if the constructed byte is the null terminator 0
+        if byte == 0:  
+            break
+        msg_bytes.append(byte)
+
+    #Convert byte array to string
+    hidden_msg = msg_bytes.decode('utf-8')
+    return hidden_msg
 
 if __name__ == "__main__":
     img_path = input("Enter the BMP image path: ")
+    output_img_path = "Hidden msg.bmp"
     secret_msg = input("Enter the message to hide: ")
-    encode_message(img_path, secret_msg)
+
+    # Hide the message in the BMP file
+    encode_message(img_path, secret_msg, output_img_path)
+
+     # Decode and verify the hidden message
+    decoded_message = decode_message(output_img_path)
+    print(f"Decoded message: {decoded_message}")
+    
+
+
+
+
+
